f4ff93e52683b5f808bd5210e137c3fb05f9cacf
WeilandJohnson <wtjohnson@students.unwsp.edu>
2020-11-10 12:25:00 -0600

    Unifinished Item

---
+++ src/src/MugAndTShirt.java
@@ -0,0 +1,17 @@
+package src;
+
+public class MugAndTShirt {
+	
+		String Item;
+		String Name;
+		double Price;
+		
+		public Item (double Price, String Name) {
+			
+		}
+		
+		public Item getName(double i) {
+			return Name;
+		}
+		//this is very bad coding
+}
