17b23be9289a726b980ebdc83e5b9dc785ecbb43
sophiaevers <sophia.m.evers@gmail.com>
2020-11-10 12:06:04 -0600

    Vendor class is complete.

---
+++ src/src/Vendor.java
@@ -0,0 +1,25 @@
+package src;
+
+public class Vendor
+{
+	String names, itemSupplied;
+	
+	public String getName()
+	{
+		return names;
+	}
+	public void setName(String name)
+	{
+		names = name;
+	}
+	public String getItemSupplied()
+	{
+		return itemSupplied;
+	}
+	public void setItemSupplied(String item)
+	{
+		itemSupplied = item;
+	}
+	
+}
+// Sophia Evers
