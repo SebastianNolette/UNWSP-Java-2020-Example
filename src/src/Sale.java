2c5eab90637c0c3526da8b2eb2e4704bf86a4b46
Aaron Castor <aaroncastorac@gmail.com>
2020-11-12 11:21:00 -0600

    Added Sale class from master branch manually

---
+++ src/src/Sale.java
@@ -0,0 +1,25 @@
+//Created by Micah Vranyes on 11/10/2020
+//
+package src;
+
+public class Sale {
+
+	private String i_item;
+	private int i_number;
+	
+	public void setItem(String item) {
+		i_item = item;
+	}
+	
+	public void setNumber(int number) {
+		i_number = number;
+	}
+	
+	public String getItem() {
+		return i_item;
+	}
+	
+	public int getNumber() {
+		return i_number;
+	}
+}
